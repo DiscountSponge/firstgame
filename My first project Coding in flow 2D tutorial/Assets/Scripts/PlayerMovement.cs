ade040a5cf1399d263f8672999e3160f9b1585c6
DiscountSponge <156786512+DiscountSponge@users.noreply.github.com>
2024-01-23 22:34:07 +0000

    fixing jumping and sticking to boundary

--- My first project Coding in flow 2D tutorial/Assets/Scripts/PlayerMovement.cs
+++ My first project Coding in flow 2D tutorial/Assets/Scripts/PlayerMovement.cs
@@ -6,21 +6,22 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D rb; //Rigidbody2D is the kind of varaiable
+    private BoxCollider2D coll; // box collider of the player
     private SpriteRenderer sprite;
     private Animator anim;
     private float dirX= 0f;
 
     [SerializeField]private float movespeed = 7f; // this allows us to change the values in unity
     [SerializeField]private float jumpforce = 14f;
-
+    [SerializeField] private LayerMask jumpableground;
     private enum MovementState {idle, Running, Jumping, Falling }
-    private MovementState state = MovementState.idle;
     // Start is called before the first frame update
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
         sprite = GetComponent<SpriteRenderer>();
+        coll = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -28,7 +29,7 @@ public class PlayerMovement : MonoBehaviour
     {
         dirX = Input.GetAxisRaw("Horizontal"); // When we set left it will be -1 and right is +1 and joysticks will have something between
         rb.velocity = new Vector2(dirX * movespeed, rb.velocity.y); //this cuts down on the amount of code needed and gives joystick support
-        if (Input.GetButtonDown("Jump")) // uses Unitys in built movement systems
+        if (Input.GetButtonDown("Jump") && IsGrounded()) // uses Unitys in built movement systems
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpforce);
         }
@@ -36,20 +37,35 @@ public class PlayerMovement : MonoBehaviour
     }
     private void UpdateAnimationUpdate()
     {
+        MovementState state;
         if (dirX > 0f) // this is running to the right
         {
-            anim.SetBool("Running", true);
+            state = MovementState.Running;
             sprite.flipX = false;
         }
         else if (dirX < 0f) // running left
         {
-            anim.SetBool("Running", true);
+            state = MovementState.Running;
             sprite.flipX = true; // flips the sprite on its x axis so it looks left
         }
         else
         {
-            anim.SetBool("Running", false);
+            state = MovementState.idle;
         }
+        if (rb.velocity.y > .1f) // Checking for jumping we cant use 0 as its never exactly zero for reasons idk 
+        {
+            state = MovementState.Jumping;
+        }
+        else if (rb.velocity.y < -.1f)
+        {
+            state = MovementState.Falling;
+        }
+        anim.SetInteger("state", (int)state);
     }
 
+    private bool IsGrounded()
+    {
+
+       return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableground);
+    }
 }
