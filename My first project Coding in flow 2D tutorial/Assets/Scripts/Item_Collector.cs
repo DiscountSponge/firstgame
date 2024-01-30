567fb42d1e3535b6e30ce5159f5bb410f4493ce6
DiscountSponge <156786512+DiscountSponge@users.noreply.github.com>
2024-01-30 20:28:47 +0000

    Moving Platform and stuff

--- My first project Coding in flow 2D tutorial/Assets/Scripts/Item_Collector.cs
+++ My first project Coding in flow 2D tutorial/Assets/Scripts/Item_Collector.cs
@@ -1,17 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
+using TMPro;
 public class Item_Collector : MonoBehaviour
 {
     private int cherries = 0;
+    public TMP_Text cherriesText;
+    private string x;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Cherry"))
         {
             Destroy(collision.gameObject);
             cherries++;
-            Debug.Log("Cherries: " + cherries);
+
+            cherriesText.text = "Cherries" + cherries;
         }
+    }
+    private void Start()
+    {
+        
+
     }
 }
